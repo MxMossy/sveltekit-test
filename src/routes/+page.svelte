e8fdbe34056d56043f120c29fd075dc6839630f0
Max Mossberg <mossberg.max@gmail.com>
2024-01-23 09:50:54 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
@@ -21,26 +20,13 @@
 		to your new<br />SvelteKit app
 	</h1>
 
-	<h2>
+	<h2 class="font-bold underline">
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
 	.welcome {
 		display: block;
 		position: relative;
